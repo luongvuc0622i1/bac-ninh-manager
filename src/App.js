f2851506bc8cbef017aed4929fc968a8e5e560f7
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-08-29 17:19:59 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { saveAs } from "file-saver";
 
 function App() {
+  const click = () => {
+    const a = {
+      "type": "Feature",
+      "geometry": {
+        "id": "BN01",
+        "name": "Bắc Ninh - Lương Tài",
+        "status": true
+      },
+      "coordinates": {
+        "go": "[bn01Go]",
+        "back": "[bn01Back]"
+      }
+    };
+
+    const data = `export let routesData = {
+  "type": "FeatureCollection",
+  "features": [
+    `+JSON.stringify(a)+`,
+    `+JSON.stringify(a)+`,
+    `+JSON.stringify(a)+`
+  ]
+}`;
+
+console.log(a);
+
+    const blob = new Blob([data], { type: "application/javascript" });
+    saveAs(blob, "data1.js");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='header'>
+        <img src='https://raw.githubusercontent.com/luongvuc0622i1/project-data/master/images/logo2.png' alt='logo' className='logo' />
+        <h1 className='title'>Bắc Ninh Bus</h1>
+      </div>
+      <div className="row">
+        <div className="col col-4">
+          {/* <form>
+            <div className="form-group">
+              <label for="inputFile">Chọn File</label>
+              <input className="form-control" id="inputFile" />
+            </div>
+            <div className="form-group">
+              {/* <label for="inputReport">Chọn mẫu Report</label> */}
+              {/* <select className="form-control" defaultValue="default" id="inputReport">
+                <option value="default" disabled hidden>Chọn kiểu Report</option>
+              </select>
+            </div> */}
+            <div className="form-group">
+              <button className="btn btn-primary" onClick={click} style={{ float: "right" }}>Xuất report</button>
+            </div>
+          {/* </form> */}
+        </div>
+      </div>
     </div>
   );
 }
